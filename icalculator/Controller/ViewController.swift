5405b9baa2c510c7199f2f2dfc9259e4f9444a88
Duy <duy@duys-mbp.home>
2018-09-23 00:57:10 -0700

    backspace, decimal buttons

--- icalculator/Controller/ViewController.swift
+++ icalculator/Controller/ViewController.swift
@@ -18,6 +18,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var display2: UILabel!
     @IBOutlet weak var display3: UILabel!
     
+    @IBAction func clearPressed(_ sender: UIButton) {
+        display1.text = "0"
+    }
+    
+    @IBAction func backSpacePressed(_ sender: UIButton) {
+        display1.text = String(display1.text!.dropLast())
+    }
+    
     @IBAction func numberPressed(_ sender: UIButton) {
         if (display1.text == "0"){
             display1.text = ""
@@ -25,23 +33,19 @@ class ViewController: UIViewController {
         display1.text = display1.text! + String(sender.tag)
     }
     
+    @IBAction func decimalPressed(_ sender: UIButton) {
+        if(!display1.text!.contains(".")) {
+            display1.text = display1.text! + "."
+        }
+    }
+    
     @IBAction func operationPressed(_ sender: UIButton) {
-    }
-    
-    @IBAction func cmpParameterPressed(_ sender: UIButton) {
-    }
-    
-    @IBAction func clearPressed(_ sender: UIButton) {
-        display1.text = "0"
-    }
-    
-    @IBAction func backSpacePressed(_ sender: UIButton) {
     }
     
     @IBAction func equalsPressed(_ sender: UIButton) {
     }
     
-    @IBAction func decimalPressed(_ sender: UIButton) {
+    @IBAction func cmpParameterPressed(_ sender: UIButton) {
     }
     
     @IBAction func changeSignPressed(_ sender: UIButton) {
