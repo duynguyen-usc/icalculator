5bd64764ce80914026b65fe3742655881bdcbf78
Duy <duy@duys-mbp.home>
2018-09-24 09:30:54 -0700

    add ui test for cmpcalc

---
+++ icalculatorUITests/UITestCmpCalc.swift
@@ -0,0 +1,27 @@
+//
+//  cmpCalcUITest.swift
+//  icalculatorUITests
+//
+//  Created by Duy on 9/24/18.
+//  Copyright © 2018 Duy. All rights reserved.
+//
+
+import XCTest
+
+class UITestCmpCalc: XCTestCase {
+
+    func testPrincipal() {
+    }
+    
+    func testInterestRate() {
+        
+    }
+    
+    func testTerm(){
+        
+    }
+    
+    func testNumTimesCompounded(){
+        
+    }
+}
